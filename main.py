5cba07a37507d15ff426e802ab33dbbba804b759
Ahmad Omid Saifi <132646206+AhmadOmidSaifi@users.noreply.github.com>
2025-06-03 22:07:23 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import turtle as turtle_module
+import random
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.hideturtle()
+color_list = [(200, 10, 35), (247, 236, 37), (240, 244, 251), (239, 231, 3), (36, 216, 77), (223, 159, 61), (39, 79, 185), (28, 39, 159), (210, 73, 16), (17, 151, 18), (239, 39, 152), (65, 9, 27)]
+tim.setheading(225)
+tim.penup()
+tim.forward(300)
+tim.setheading(0)
+
+
+number_of_dots = 100
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import colorgram
+#colors = colorgram.extract('image.jpg',15)
+
+#colrs = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     rgb_colrs = (r, g, b)
+#     colrs.append(rgb_colrs)
+# print(colrs)
